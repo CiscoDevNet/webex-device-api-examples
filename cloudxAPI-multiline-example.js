aee49a2bd9a8836684b32e97208d7a2ebe96e62c
Richard Bayes <rich@rbayes.com>
2020-07-28 20:06:36 +0200

    Update cloudxAPI-multiline-example.js

--- cloudxAPI-multiline-example.js
+++ cloudxAPI-multiline-example.js
@@ -1,35 +1,31 @@
 //NodeJS Example on how to post multiline commands to Webex Devices that allow cloud xapi commands"
-
 const axios = require("axios");
 
 const ACCESS_TOKEN =
-  "enter your access token here";
+    "enter your access token here";
 
 const DEVICE_ID =
-   "enter your deviceID here";
+    "enter your deviceID here";
 
 const jsonConfig = {
-  headers: {
-    "User-Agent": "Mozilla/5.0",
-    "Authorization": `Bearer ${ACCESS_TOKEN}`,
-    "Content-Type": "application/json",
-  },
+    headers: {
+        "User-Agent": "Mozilla/5.0",
+        "Authorization": `Bearer ${ACCESS_TOKEN}`,
+        "Content-Type": "application/json",
+    },
 };
 
-const commandData = JSON.stringify(
-  {
-	"deviceId": `${DEVICE_ID}`,
-	"arguments": {
-	},
-	"body": "Webex Rooms Device\nAuthorised Access Only"
-  },
-);
+const commandData = JSON.stringify({
+    "deviceId": `${DEVICE_ID}`,
+    "arguments": {},
+    "body": "Webex Rooms Device\nAuthorised Access Only"
+}, );
 
 axios.post('https://webexapis.com/v1/xapi/command/SystemUnit.WelcomeBanner.Set', commandData, jsonConfig)
-.then((res) => {
-  console.log(`statusCode: ${res.statusCode}`)
-  console.log(res)
-})
-.catch((error) => {
-  console.error(error)
-})
+    .then((res) => {
+        console.log(`statusCode: ${res.statusCode}`)
+        console.log(res)
+    })
+    .catch((error) => {
+        console.error(error)
+    })
