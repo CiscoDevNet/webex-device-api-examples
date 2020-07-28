ee164910acd153038823548d31f19474f210429b
Richard Bayes <rich@rbayes.com>
2020-07-28 13:21:06 +0200

    Create cloudxAPI-multiline-example.js
    This example shows how to use the new multiline functionality built into the Cloud xAPI for xapi enabled devices.
    
    https://developer.webex.com/docs/api/v1/xapi/execute-command
    
    See the above link for more detail on other ways to use this endpoint

---
+++ cloudxAPI-multiline-example.js
@@ -0,0 +1,8 @@
+//Body for "https://webexapis.com/v1/xapi/command/SystemUnit.WelcomeBanner.Set"
+
+{
+	"deviceId": "<deviceId>",
+	"arguments": {
+	},
+	"body": "Webex Rooms Device\nAuthorised Access Only"
+}
