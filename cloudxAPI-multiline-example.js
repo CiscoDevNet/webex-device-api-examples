3afdb7394e6da7d280ee58f4b13dfc32d775f599
Richard Bayes <rich@rbayes.com>
2020-07-28 13:48:20 +0200

    Update cloudxAPI-multiline-example.js

--- cloudxAPI-multiline-example.js
+++ cloudxAPI-multiline-example.js
@@ -1,8 +1,35 @@
 //Body for "https://webexapis.com/v1/xapi/command/SystemUnit.WelcomeBanner.Set"
 
-{
-	"deviceId": "<deviceId>",
+const axios = require("axios");
+
+const ACCESS_TOKEN =
+  "enter your access token here";
+
+const DEVICE_ID =
+   "enter your deviceID here";
+
+const jsonConfig = {
+  headers: {
+    "User-Agent": "Mozilla/5.0",
+    "Authorization": `Bearer ${ACCESS_TOKEN}`,
+    "Content-Type": "application/json",
+  },
+};
+
+const commandData = JSON.stringify([
+  {
+	"deviceId": `${DEVICE_ID}`,
 	"arguments": {
 	},
 	"body": "Webex Rooms Device\nAuthorised Access Only"
-}
+  },
+]);
+
+axios.post('https://webexapis.com/v1/xapi/command/SystemUnit.WelcomeBanner.Set', jsonConfig, commandData)
+.then((res) => {
+  console.log(`statusCode: ${res.statusCode}`)
+  console.log(res)
+})
+.catch((error) => {
+  console.error(error)
+})
